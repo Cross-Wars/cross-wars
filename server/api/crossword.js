11aafbb595f1d5b080b00e50850b48b884c70ac0
dipti <dipti.yadav@outlook.com>
2022-03-23 11:20:49 -0400

    Crossword api route

---
+++ server/api/crossword.js
@@ -0,0 +1,14 @@
+const crosswordRouter = require("express").Router()
+const { Crossword } = require("../db")
+
+crosswordRouter.get("/", async (req, res, next) => {
+  try {
+    const crosswords = await Crossword.findAll()
+    console.log(crosswords)
+    res.send(crosswords)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = crosswordRouter
